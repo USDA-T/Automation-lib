d6435ec33ec434d83144582940d675a069c6c80c
burinc <bchoomnuan@enquizit.com>
2017-04-13 08:06:47 -0400

    Add more wrapper and automate some WOSB steps
    - Use Google style guide for the project

--- src/main/java/gov/sba/automation/AutomationUtils.java
+++ src/main/java/gov/sba/automation/AutomationUtils.java
@@ -8,115 +8,115 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutomationUtils {
-    
-    private static final Logger logger = LoggerFactory.getLogger(AutomationUtils.class);
 
-    private WebDriverWait wdriver;
-    private WebDriver driver;
+  private static final Logger logger = LoggerFactory.getLogger(AutomationUtils.class);
 
-    public AutomationUtils() {
+  private WebDriverWait wdriver;
+  private WebDriver driver;
 
-        BrowserUtils browserUtils = new BrowserUtils();
-        browserUtils.initFirefox();
+  public AutomationUtils() {
 
-        wdriver = browserUtils.getWaitDriver();
-        driver = browserUtils.getWebDriver();
+    BrowserUtils browserUtils = new BrowserUtils();
+    browserUtils.initFirefox();
+
+    wdriver = browserUtils.getWaitDriver();
+    driver = browserUtils.getWebDriver();
+  }
+
+  public WebDriverWait getWdriver() {
+    return wdriver;
+  }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
+
+  public void login(String username, String password) {
+    driver.findElement(By.cssSelector(".button-full")).click();
+    wdriver.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#user_email")));
+
+    driver.findElement(By.cssSelector("#user_email")).sendKeys(username);
+    driver.findElement(By.cssSelector("#user_password")).sendKeys(password);
+    driver.findElement(By.cssSelector("#business_signin")).click();
+  }
+
+  public void startNewProgram(String programName) throws Exception {
+    driver.get("http://localhost:3000");
+
+    CoreWait.waitForPresenceOfElementLocated(wdriver, By.cssSelector(".button-full"));
+
+    login("john@mailinator.com", "password");
+
+    By locator = By.partialLinkText("Programs");
+    CoreWait.waitForElementToBeClickable(driver, wdriver, locator);
+
+    String actual = driver.findElement(locator).getText();
+    CoreUtils.assertContentEquals("Programs", actual);
+
+    // Now let's click the program link
+    CoreUtils.locateAndClick(driver, locator);
+
+    boolean deleteDraft = false;
+
+    if (deleteDraft) {
+      logger.debug("FYI: about to call deleteDrafProgram()");
+      // TODO: delete the 'Draft' application if any?
+      CoreUtils.deleteDraftProgram(driver, programName);
+      Thread.sleep(5000);
     }
-	
-    public WebDriverWait getWdriver() {
-		return wdriver;
-	}
 
-	public WebDriver getDriver() {
-		return driver;
-	}
-    
-    public void login(String username, String password) {
-        driver.findElement(By.cssSelector(".button-full")).click();
-        wdriver.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#user_email")));
+    // Get the program name
+    String programDesc = CoreUtils.lookupProgram(programName);
+    logger.debug("FYI: using : " + programDesc);
 
-        driver.findElement(By.cssSelector("#user_email")).sendKeys(username);
-        driver.findElement(By.cssSelector("#user_password")).sendKeys(password);
-        driver.findElement(By.cssSelector("#business_signin")).click();
-    }
+    // Need to click "#certificate_type_" + programName here
+    locator = By.cssSelector("#certificate_type_" + programName);
+    CoreUtils.locateAndClick(driver, locator);
 
-    public void startNewProgram(String programName) throws Exception {
-        driver.get("http://localhost:3000");
+    locator = By.cssSelector("#add_certification");
+    CoreUtils.locateAndClick(driver, locator);
 
-        CoreWait.waitForPresenceOfElementLocated(wdriver, By.cssSelector(".button-full"));
-        
-        login("john@mailinator.com", "password");
+    // Then we click Continue button
+    CoreUtils.clickContinue(driver);
+  }
 
-        By locator = By.partialLinkText("Programs");
-        CoreWait.waitForElementToBeClickable(driver, wdriver, locator);
+  public void clickThroughWosb(WebDriver driver) {
+    CoreUtils.yesOrNo(driver, "no", new int[] {198});
+    CoreUtils.clickContinue(driver);
 
-        String actual = driver.findElement(locator).getText();
-        CoreUtils.assertContentEquals("Programs", actual);
-        
-        // Now let's click the program link
-        CoreUtils.locateAndClick(driver, locator);
-        
-        boolean deleteDraft = false;
-        
-        if (deleteDraft) {
-        	logger.debug("FYI: about to call deleteDrafProgram()");
-        	// TODO: delete the 'Draft' application if any?
-        	CoreUtils.deleteDraftProgram(driver, programName);
-        	Thread.sleep(5000);
-        }
-        
-        // Get the program name
-        String programDesc = CoreUtils.lookupProgram(programName);
-        logger.debug("FYI: using : " + programDesc);
-        
-        // Need to click "#certificate_type_" + programName here
-        locator = By.cssSelector("#certificate_type_" + programName);
-        CoreUtils.locateAndClick(driver, locator);
-        
-        locator = By.cssSelector("#add_certification");
-        CoreUtils.locateAndClick(driver, locator);
-        
-        // Then we click Continue button
-        CoreUtils.clickContinue(driver);
-    }
-    
-    public void clickThroughWosb(WebDriver driver) {
-    	CoreUtils.yesOrNo(driver, "no", new int[] {198});
-    	CoreUtils.clickContinue(driver);
+    CoreUtils.yesOrNo(driver, "no", new int[] {199});
+    CoreUtils.clickContinue(driver);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {199});
-    	CoreUtils.clickContinue(driver);
+    CoreUtils.yesOrNo(driver, "no", new int[] {201});
+    CoreUtils.clickContinue(driver);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {201});
-    	CoreUtils.clickContinue(driver);
+    CoreUtils.yesOrNo(driver, "no", new int[] {211});
+    CoreUtils.comment(driver, 211);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {211});
-    	CoreUtils.comment(driver, 211);
+    CoreUtils.yesOrNo(driver, "no", new int[] {212});
+    CoreUtils.comment(driver, 212);
+    CoreUtils.clickContinue(driver);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {212});
-    	CoreUtils.comment(driver, 212);
-    	CoreUtils.clickContinue(driver);
+    CoreUtils.yesOrNo(driver, "no", new int[] {213});
+    CoreUtils.clickContinue(driver);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {213});
-    	CoreUtils.clickContinue(driver);
-    	
-    	CoreUtils.yesOrNo(driver, "no", new int[] {214, 215, 216});
-    	CoreUtils.comment(driver, 216);
-    	CoreUtils.clickContinue(driver);
+    CoreUtils.yesOrNo(driver, "no", new int[] {214, 215, 216});
+    CoreUtils.comment(driver, 216);
+    CoreUtils.clickContinue(driver);
 
-    	CoreUtils.yesOrNo(driver, "no", new int[] {217, 218, 219, 220, 221, 222});
-    	CoreUtils.comment(driver, 222);
-    	CoreUtils.clickContinue(driver);
-    	
-    	CoreUtils.yesOrNo(driver, "no", new int[] {223});
-    	CoreUtils.clickContinue(driver);
-    }
-    
-    public static void main(String... args) throws Exception {
-        AutomationUtils app = new AutomationUtils();
-        app.startNewProgram("wosb");
-        
-        app.clickThroughWosb(app.getDriver());
-        System.out.println("Done!");
-    }
+    CoreUtils.yesOrNo(driver, "no", new int[] {217, 218, 219, 220, 221, 222});
+    CoreUtils.comment(driver, 222);
+    CoreUtils.clickContinue(driver);
+
+    CoreUtils.yesOrNo(driver, "no", new int[] {223});
+    CoreUtils.clickContinue(driver);
+  }
+
+  public static void main(String... args) throws Exception {
+    AutomationUtils app = new AutomationUtils();
+    app.startNewProgram("wosb");
+
+    app.clickThroughWosb(app.getDriver());
+    System.out.println("Done!");
+  }
 }
--- src/main/java/gov/sba/automation/CoreUtils.java
+++ src/main/java/gov/sba/automation/CoreUtils.java
@@ -18,120 +18,120 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Function;
 
 public class CoreUtils {
-    private static final Logger logger = LoggerFactory.getLogger(CoreUtils.class);
-    
-    /**
-     * Poll for element until the element is presented or timeout
-     */
-    public static WebElement waitForElement(WebDriver driver, By locator) {
-        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver);
-        wait.pollingEvery(250, TimeUnit.MILLISECONDS);
-        wait.withTimeout(10, TimeUnit.SECONDS);
-        wait.ignoring(NoSuchElementException.class);
+  private static final Logger logger = LoggerFactory.getLogger(CoreUtils.class);
 
-        WebElement element = wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver d) {
-                return d.findElement(locator);
-            }
-        });
+  /**
+   * Poll for element until the element is presented or timeout
+   */
+  public static WebElement waitForElement(WebDriver driver, By locator) {
+    FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver);
+    wait.pollingEvery(250, TimeUnit.MILLISECONDS);
+    wait.withTimeout(10, TimeUnit.SECONDS);
+    wait.ignoring(NoSuchElementException.class);
 
-        return element;
+    WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+      public WebElement apply(WebDriver d) {
+        return d.findElement(locator);
+      }
+    });
+
+    return element;
+  }
+
+  public static void locateAndClick(WebDriver driver, By locator) {
+    WebElement element = waitForElement(driver, locator);
+    element.click();
+  }
+
+  public static void locateAndSubmit(WebDriver driver, By locator) {
+    WebElement element = waitForElement(driver, locator);
+    element.submit();
+  }
+
+  public static void locateAndFill(WebDriver driver, By locator, String text) {
+    WebElement element = waitForElement(driver, locator);
+    element.sendKeys(text);
+  }
+
+  public static void yesOrNo(WebDriver driver, String answerType, int[] ids) {
+    String expr;
+    for (int id : ids) {
+      expr = "#answers_" + id + "_value_" + answerType;
+      locateAndClick(driver, By.cssSelector(expr));
     }
+  }
 
-    public static void locateAndClick(WebDriver driver, By locator) {
-        WebElement element = waitForElement(driver, locator);
+  public static void comment(WebDriver driver, int id) {
+    String expr = "#answers_" + id + "_comment";
+    locateAndFill(driver, By.cssSelector(expr), "Comment for : " + id);
+  }
+
+  public static void clickContinue(WebDriver driver) {
+    locateAndSubmit(driver, By.name("commit"));
+  }
+
+  public static void accepTermsAndConditions(WebDriver driver, int[] ids) {
+    for (int id : ids) {
+      locateAndClick(driver, By.cssSelector("#legal_" + id));
+    }
+  }
+
+  // Utility functions
+  public static void assertContentEquals(String expected, String actual) {
+    if (!(expected.equals(actual))) {
+      throw new RuntimeException("Expected: " + expected + ", Actual: " + actual);
+    }
+  }
+
+  private static final Map<String, String> programsMap;
+
+  static {
+    Map<String, String> aMap = new HashMap<String, String>();
+
+    aMap.put("mpp", "MPP");
+    aMap.put("wosb", "WOSB Self-Certification");
+    aMap.put("eight_a", "8(a) Document Upload");
+    aMap.put("edwosg", "EDWOSB");
+
+    programsMap = Collections.unmodifiableMap(aMap);
+  }
+
+  public static String lookupProgram(String programName) {
+    return programsMap.get(programName);
+  }
+
+  public static void deleteDraftProgram(WebDriver driver, String programName) {
+    logger.info("FYI: looking up using programName : " + programName);
+
+    String programDesc = CoreUtils.lookupProgram(programName);
+    logger.info("FYI: looking up using programDesc : " + programDesc);
+
+    try {
+      // xpath expression for 'Draft expression.."
+      String xpathExpr = String.format(
+          "//*[@id='certifications']/tbody/tr[(td[position()=1]/a[contains(text(),'%s')]) and (td[position()=5 and contains(text(),'Draft')])]/td[position()=7]/a[ contains(text(),'Delete')]",
+          programDesc);
+
+      By locator = By.xpath(xpathExpr);
+
+      WebElement element = driver.findElement(locator);
+
+      if (element != null) {
+        logger.debug("FYI: element found, and will be click: " + element);
         element.click();
-    }
-
-    public static void locateAndSubmit(WebDriver driver, By locator) {
-        WebElement element = waitForElement(driver, locator);
-        element.submit();
-    }
-
-    public static void locateAndFill(WebDriver driver, By locator, String text) {
-        WebElement element = waitForElement(driver, locator);
-        element.sendKeys(text);
-    }
-
-    public static void yesOrNo(WebDriver driver, String answerType, int[] ids) {
-        String expr;
-        for (int id : ids) {
-            expr = "#answers_" + id + "_value_" + answerType;
-            locateAndClick(driver, By.cssSelector(expr));
-        }
-    }
-
-    public static void comment(WebDriver driver, int id) {
-        String expr = "#answers_" + id + "_comment";
-        locateAndFill(driver, By.cssSelector(expr), "Comment for : " + id);
-    }
-
-    public static void clickContinue(WebDriver driver) {
-        locateAndSubmit(driver, By.name("commit"));
-    }
-
-    public static void accepTermsAndConditions(WebDriver driver, int[] ids) {
-        for (int id : ids) {
-            locateAndClick(driver, By.cssSelector("#legal_" + id));
-        }
-    }
-
-    // Utility functions
-    public static void assertContentEquals(String expected, String actual) {
-        if (!(expected.equals(actual))) {
-            throw new RuntimeException("Expected: " + expected + ", Actual: " + actual);
-        }
-    }
-
-    private static final Map<String, String> programsMap;
-
-    static {
-        Map<String, String> aMap = new HashMap<String, String>();
-
-        aMap.put("mpp", "MPP");
-        aMap.put("wosb", "WOSB Self-Certification");
-        aMap.put("eight_a", "8(a) Document Upload");
-        aMap.put("edwosg", "EDWOSB");
-
-        programsMap = Collections.unmodifiableMap(aMap);
-    }
-
-    public static String lookupProgram(String programName) {
-       return programsMap.get(programName);
-    }
-
-    public static void deleteDraftProgram(WebDriver driver, String programName) {    
-        logger.info("FYI: looking up using programName : " + programName);
-        
-        String programDesc = CoreUtils.lookupProgram(programName);
-        logger.info("FYI: looking up using programDesc : " + programDesc);
-        
-        try {
-            // xpath expression for 'Draft expression.."
-            String xpathExpr = String.format("//*[@id='certifications']/tbody/tr[(td[position()=1]/a[contains(text(),'%s')]) and (td[position()=5 and contains(text(),'Draft')])]/td[position()=7]/a[ contains(text(),'Delete')]",
-                    programDesc
-                    );
-            
-            By locator = By.xpath(xpathExpr);
-
-            WebElement element = driver.findElement(locator);
-
-            if (element != null) {
-                logger.debug("FYI: element found, and will be click: " + element);
-                element.click();
-                driver.switchTo().alert().accept();
-            } else {
-                logger.debug("FYI: element not found, no action will be taken!");
-            }
-        } catch (Exception e) {
-            logger.warn("FYI: exceptions .." + e.getMessage());
-            // NOTE: this is to be expected if we don't have any 'Draft' certification on the first run
-        }
-    }
-
-    public static void acceptPopupAlert(WebDriver driver) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.alertIsPresent());
         driver.switchTo().alert().accept();
-    }    
+      } else {
+        logger.debug("FYI: element not found, no action will be taken!");
+      }
+    } catch (Exception e) {
+      logger.warn("FYI: exceptions .." + e.getMessage());
+      // NOTE: this is to be expected if we don't have any 'Draft' certification on the first run
+    }
+  }
+
+  public static void acceptPopupAlert(WebDriver driver) {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.alertIsPresent());
+    driver.switchTo().alert().accept();
+  }
 }
