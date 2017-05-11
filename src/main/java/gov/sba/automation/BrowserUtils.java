531065418bd7f4fa51f91aea0182d6ebf8175aa5
Burin Choomnuan <bchoomnuan@enquizit.com>
2017-05-11 15:27:50 -0400

    Move re-usable code to the library

--- src/main/java/gov/sba/automation/BrowserUtils.java
+++ src/main/java/gov/sba/automation/BrowserUtils.java
@@ -9,48 +9,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BrowserUtils {
-  private static final Logger logger = LoggerFactory.getLogger(BrowserUtils.class);
+	private static final Logger logger = LoggerFactory.getLogger(BrowserUtils.class);
 
-  private WebDriverWait wait;
-  private WebDriver driver;
+	private WebDriverWait wait;
+	private WebDriver driver;
 
-  public BrowserUtils() {
-    initFirefox();
-    // TODO: allow Chrome driver here if you like
-    driver = new FirefoxDriver();
+	public BrowserUtils() {
+		initFirefox();
+		// TODO: allow Chrome driver here if you like
+		driver = new FirefoxDriver();
 
-    // Some sensible default for testing
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// Some sensible default for testing
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-    // wait for at most 10 seconds before timeout
-    wait = new WebDriverWait(driver, 10);
-  }
+		// wait for at most 10 seconds before timeout
+		wait = new WebDriverWait(driver, 10);
+	}
 
-  public WebDriverWait getWaitDriver() {
-    return wait;
-  }
+	public WebDriverWait getWaitDriver() {
+		return wait;
+	}
 
-  public WebDriver getWebDriver() {
-    return driver;
-  }
+	public WebDriver getWebDriver() {
+		return driver;
+	}
 
-  public static boolean isWindow() {
-    return System.getProperty("os.name").toLowerCase().contains("windows");
-  }
+	public static boolean isWindow() {
+		return System.getProperty("os.name").toLowerCase().contains("windows");
+	}
 
-  // Note: support just Firefox during development to simplify the code
-  public void initFirefox() {
-    String driverPath = System.getenv("HOME") + "/apps/geckodriver";
-    logger.info("FYI: using driverPath : " + driverPath);
+	// Note: support just Firefox during development to simplify the code
+	public void initFirefox() {
+		String driverPath = System.getenv("HOME") + "/apps/geckodriver";
+		logger.info("FYI: using driverPath : " + driverPath);
 
-    if (isWindow()) {
-      driverPath += ".exe";
-    }
+		if (isWindow()) {
+			driverPath += ".exe";
+		}
 
-    System.setProperty("webdriver.firefox.driver", driverPath);
-    System.setProperty("webdriver.gecko.driver", driverPath);
-    System.setProperty("webdriver.firefox.port", "7046");
-  }
+		System.setProperty("webdriver.firefox.driver", driverPath);
+		System.setProperty("webdriver.gecko.driver", driverPath);
+		System.setProperty("webdriver.firefox.port", "7046");
+	}
 
 }
