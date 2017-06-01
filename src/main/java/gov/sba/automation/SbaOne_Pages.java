8667df8376e94c56f4d506ea0e29cecb7c5c845e
deepapatri <deepa.patri.ctr@telesishq.com>
2017-06-01 00:28:52 -0400

    Masster

--- src/main/java/gov/sba/automation/SbaOne_Pages.java
+++ src/main/java/gov/sba/automation/SbaOne_Pages.java
@@ -5,7 +5,5 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SbaOne_Pages {
-
 	private static final Logger sbaOne_Page_Logs = LogManager.getLogger(SbaOne_Pages.class.getName());
-
 }
